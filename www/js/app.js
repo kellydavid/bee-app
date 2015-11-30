b9d37d0c95ed2346a27d5014b543661ad540be56
kellydavid <kellydavid178@gmail.com>
2015-11-30 21:23:52 +0000

    Added analysis tab

--- www/js/app.js
+++ www/js/app.js
@@ -40,6 +40,16 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
 
   // Each tab has its own nav history stack:
 
+  .state('tab.analysis-filters', {
+    url: '/analysis',
+    views: {
+      'analysis-filters': {
+        templateUrl: 'templates/analysis-filters.html',
+        controller: 'AnalysisCtrl'
+      }
+    }
+  })
+
   .state('tab.dash', {
     url: '/dash',
     views: {
