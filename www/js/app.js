cdb736ef1a3f09e1d8b3e5d441ae179fff8d2317
kellydavid <kellydavid178@gmail.com>
2015-11-30 22:58:46 +0000

    Renamed template tabs for bee app

--- www/js/app.js
+++ www/js/app.js
@@ -40,56 +40,47 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
 
   // Each tab has its own nav history stack:
 
-  .state('tab.analysis-filters', {
+  .state('tab.tasks', {
+    url: '/tasks',
+    views: {
+      'tasks': {
+        templateUrl: 'templates/tasks.html',
+        controller: 'TasksCtrl'
+      }
+    }
+  })
+
+  .state('tab.sort', {
+      url: '/sort',
+      views: {
+        'sort': {
+          templateUrl: 'templates/sort.html',
+          controller: 'SortCtrl'
+        }
+      }
+    })
+
+  .state('tab.profile', {
+    url: '/profile',
+    views: {
+      'profile': {
+        templateUrl: 'templates/profile.html',
+        controller: 'ProfileCtrl'
+      }
+    }
+  })
+
+   .state('tab.analysis', {
     url: '/analysis',
     views: {
-      'analysis-filters': {
-        templateUrl: 'templates/analysis-filters.html',
+      'analysis': {
+        templateUrl: 'templates/analysis.html',
         controller: 'AnalysisCtrl'
-      }
-    }
-  })
-
-  .state('tab.dash', {
-    url: '/dash',
-    views: {
-      'tab-dash': {
-        templateUrl: 'templates/tab-dash.html',
-        controller: 'DashCtrl'
-      }
-    }
-  })
-
-  .state('tab.chats', {
-      url: '/chats',
-      views: {
-        'tab-chats': {
-          templateUrl: 'templates/tab-chats.html',
-          controller: 'ChatsCtrl'
-        }
-      }
-    })
-    .state('tab.chat-detail', {
-      url: '/chats/:chatId',
-      views: {
-        'tab-chats': {
-          templateUrl: 'templates/chat-detail.html',
-          controller: 'ChatDetailCtrl'
-        }
-      }
-    })
-
-  .state('tab.account', {
-    url: '/account',
-    views: {
-      'tab-account': {
-        templateUrl: 'templates/tab-account.html',
-        controller: 'AccountCtrl'
       }
     }
   });
 
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/tab/dash');
+  $urlRouterProvider.otherwise('/tab/tasks');
 
 });
