036bbf653342697c243f720b9e8d67b4e078a60d
kellydavid <kellydavid178@gmail.com>
2015-12-07 00:20:34 +0000

    Fixed the scroll selection for ios

--- www/js/app.js
+++ www/js/app.js
@@ -12,6 +12,7 @@ angular.module('starter', ['ionic', 'chart.js', 'starter.controllers', 'starter.
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
   if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+    cordova.plugins.Keyboard.hideKeyboardAccessoryBar(false);
     cordova.plugins.Keyboard.disableScroll(true);
   }
   if (window.StatusBar) {
